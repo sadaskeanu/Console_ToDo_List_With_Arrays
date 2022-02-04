05b0ec9a2bacf7828c8c94b731aed732de4d630a
Daria <79363439+sadaskeanu@users.noreply.github.com>
2022-02-04 22:42:59 +0200

    Add files via upload

---
+++ todo_arr.js
@@ -0,0 +1,79 @@
+const list = [
+    {task:"walk dog", status: "In Progress", priority: "high"},
+    {task: "read book", status: "To Do", priority: "low"},
+    {task: "work", status: "Done", priority: "high"},
+    ];
+    
+    const STATUS = {
+    To_Do: "To Do",
+    In_Progress: "In Progress",
+    Done: "Done",
+    } 
+    
+    const PRIORITY = {
+    High: "high",
+    Low: "low",
+    }
+    
+    function addTask (task) {
+    list.push({task, status: STATUS.To_Do, priority: PRIORITY.Low});
+    }
+
+    function deleteTask (task) {
+        list.find(function(item,index) {
+            if (item.task === task) {
+                list.splice(index,1);
+            }
+        });
+    }
+  
+    function changeStatus(task, status){
+        list.find(function(item){
+            if (item.task === task) {
+                item.status = status;
+            }
+        });
+    }
+
+    function changePriority(task, priority){
+        list.find(function(item){
+            if (item.task === task) {
+                item.priority = priority;
+            }
+        });
+    }
+
+    function showList() {
+
+        console.log("To Do:")
+        list.forEach(function(item){
+            if (item.status === STATUS.To_Do) {
+                console.log(item.task);
+            }
+        });
+
+        console.log("In Progress:")
+        list.forEach(function(item){
+            if (item.status === STATUS.In_Progress) {
+                console.log(item.task);
+            }
+        });
+
+        console.log("Done:")
+        list.forEach(function(item){
+            if (item.status === STATUS.Done) {
+                console.log(item.task);
+            }
+        });
+    }
+           
+    
+    addTask ("homework");
+    console.log(list);
+    changeStatus ("homework", "Done");
+    console.log(list);
+    changePriority ("homework", "high");
+    console.log(list);
+    deleteTask("homework");
+    console.log(list);
+    showList();
